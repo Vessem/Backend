bc08aaca936e3bba1cedc6a72db1a2ea3e1496b9
Thijn Smulders <thijnsmulders04@gmail.com>
2024-02-04 22:53:03 +0100

    Updated user table and added more user routes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import csrf from 'csurf';
 import cors from 'cors';
 import helmet from 'helmet';
+import session from 'express-session';
 // Routers
 import * as user from './routes/UserRouter';
 
@@ -12,19 +13,35 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-// Configure api
+// Configure security
 app.use(
 	cors({
 		origin: '*',
 		optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 	}),
 );
+app.use(helmet());
+app.use(helmet.xssFilter());
+app.use(helmet.frameguard());
+app.use(helmet.hsts({ maxAge: 7776000000, includeSubDomains: true }));
+app.use(helmet.hidePoweredBy());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+
+// Other configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(csrf({ cookie: true }));
-app.use(helmet());
-app.disable('x-powered-by');
+app.use(session({ secret: process.env.COOKIE_SECRET || 'SECRET', saveUninitialized: true, resave: true }));
+//app.use(csrf());
+
+// Add csrf token to requests
+// app.use(function (req, res, next) {
+// 	const token = req.csrfToken();
+// 	res.cookie('XSRF-TOKEN', token);
+// 	res.locals.csrfToken = token;
+// 	next();
+// });
 
 // Configure routes
 app.use('/users', user.default);
